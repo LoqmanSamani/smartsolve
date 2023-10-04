3835a7124eac2f12a41d32c207806a4d4f57d105
LoqmanSamani <samaniloqman91@gmail.com>
2023-10-04 19:08:00 +0200

    a test file has been added

---
+++ tests/test_neural_network.py
@@ -0,0 +1,85 @@
+import unittest
+from unittest import TestCase
+
+
+class TestNeuralNetwork(TestCase):
+    def test_input_layer_neurons(self):
+        self.fail()
+
+    def test_output_layer_neurons(self):
+        self.fail()
+
+    def test_hidden_layer_neurons(self):
+        self.fail()
+
+    def test_initialize_weight(self):
+        self.fail()
+
+    def test_initialze_bias(self):
+        self.fail()
+
+    def test_forward_propagation(self):
+        self.fail()
+
+    def test_linear_transformation(self):
+        self.fail()
+
+    def test_activation_function(self):
+        self.fail()
+
+    def test_sigmoid_activation(self):
+        self.fail()
+
+    def test_tanh_activation(self):
+        self.fail()
+
+    def test_relu_activation(self):
+        self.fail()
+
+    def test_elu_activation(self):
+        self.fail()
+
+    def test_softmax_activation(self):
+        self.fail()
+
+    def test_cost_function(self):
+        self.fail()
+
+    def test_cross_entropy(self):
+        self.fail()
+
+    def test_mean_squared_error(self):
+        self.fail()
+
+    def test_back_propagation(self):
+        self.fail()
+
+    def test_activation_derivative(self):
+        self.fail()
+
+    def test_sigmoid_derivative(self):
+        self.fail()
+
+    def test_tanh_derivative(self):
+        self.fail()
+
+    def test_relu_derivative(self):
+        self.fail()
+
+    def test_elu_derivative(self):
+        self.fail()
+
+    def test_softmax_derivative(self):
+        self.fail()
+
+    def test_train(self):
+        self.fail()
+
+    def test_predict(self):
+        self.fail()
+
+    def test_save_model(self):
+        self.fail()
+
+    def test_load_model(self):
+        self.fail()
